e5a4af7064ecb237e23a9314f974e5a9e5ccfd46
diamond <diamonddewan05@gmail.com>
2024-11-28 15:58:08 +0600

    chore: remove nuxt ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,9 +24,7 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'light'
   },
-  extends: [
-    '@nuxt/ui-pro'
-  ],
+  extends: [],
   modules: [
     '@nuxtjs/google-fonts',
     '@nuxt/ui',
